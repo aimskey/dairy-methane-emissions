ae8b5737e4947f8f46fecd4d961bd41095d4c2d7
aimskey <78832323+aimskey@users.noreply.github.com>
2021-05-06 16:53:13 -0400

    Update state_emissions.py

--- state_emissions.py
+++ state_emissions.py
@@ -24,8 +24,14 @@ digester_db.fillna(0, inplace=True)
 dtype_cownum = {'State':str,'Dairy Calves':float, 'Dairy Repl. Heif. 7-11 Months':float, 'Dairy Repl. Heif. 12-23 Months':float}
 cow_num = pd.read_csv('cow_num_by_state_thou.csv', dtype = dtype_cownum)
 cow_num["Dairy Cows"] = cow_num["Dairy Cows"].str.replace(",","").astype(float)
+cow_num["State"] = cow_num["State"].map(us_state_abbrev)
 cow_num = cow_num[['State', 'Dairy Calves','Dairy Cows', 'Dairy Repl. Heif. 7-11 Months','Dairy Repl. Heif. 12-23 Months']]
+cow_num.set_index('State', inplace=True)
 cow_num['Replacements'] = cow_num['Dairy Repl. Heif. 7-11 Months'] + cow_num['Dairy Repl. Heif. 12-23 Months']
+cow_num.drop(['Dairy Repl. Heif. 7-11 Months','Dairy Repl. Heif. 12-23 Months'], axis="columns", inplace=True)
+cow_num["total"] = cow_num.sum(axis="columns")
+
+
 
 
 
