c335ce297cf43a4bdf18246808be4341dda07c8c
aimskey <78832323+aimskey@users.noreply.github.com>
2021-05-07 07:55:26 -0400

    Update reduction_potential.py

--- reduction_potential.py
+++ reduction_potential.py
@@ -1,5 +1,15 @@
 import pandas as pd
 
+us_state_abbrev = {'Alabama': 'AL', 'Alaska': 'AK', 'Arizona': 'AZ', 'Arkansas': 'AR', 'California': 'CA', 'Colorado': 'CO',
+'Connecticut': 'CT', 'Delaware': 'DE', 'Distict of Columbia': 'DC', 'Florida': 'FL', 'Georgia': 'GA', 'Hawaii': 'HI', 'Idaho': 'ID',
+'Illinois': 'IL', 'Indiana': 'IN', 'Iowa': 'IA', 'Kansas': 'KS', 'Kentucky': 'KY', 'Louisiana': 'LA',
+'Maine': 'ME', 'Maryland': 'MD', 'Massachusetts': 'MA', 'Michigan': 'MI', 'Minnesota': 'MN', 'Mississippi': 'MS',
+'Missouri': 'MO', 'Montana': 'MT', 'Nebraska': 'NE', 'Nevada': 'NV', 'New Hampshire': 'NH', 'New Jersey': 'NJ',
+'New Mexico': 'NM', 'New York': 'NY', 'North Carolina': 'NC', 'North Dakota': 'ND', 'Ohio': 'OH', 'Oklahoma': 'OK',
+'Oregon': 'OR', 'Pennsylvania': 'PA', 'Puerto Rico': 'PR', 'Rhode Island': 'RI', 'South Carolina': 'SC', 'South Dakota': 'SD',
+'Tennessee': 'TN', 'Texas': 'TX', 'Utah': 'UT', 'Vermont': 'VT', 'Virginia': 'VA', 'Washington': 'WA',
+'West Virginia': 'WV', 'Wisconsin': 'WI', 'Wyoming': 'WY', 'United States':'US'}
+
 dtype_digester = {'State':str, 'Status':str, 'Animal/Farm Type(s)':str, 'Dairy':float, 'Total Emission Reductions(MTCO2e/yr)':float, 'Awarded USDA Funding?':str}
 digester_db = pd.read_csv("agstar-livestock-ad-database.csv", dtype=dtype_digester)
 digester_db.columns = digester_db.columns.to_series().apply(lambda x: x.strip())
@@ -10,10 +20,18 @@ digester_db.query("Status == 'Operational' and farm_type == 'Dairy'", inplace=Tr
 digester_db["usda_fund"].fillna("N", inplace=True)
 digester_db.dropna(axis="rows", inplace=True)
 
+methane_percow = pd.read_csv('em_percow.csv')
+methane_percow['State'] = methane_percow['State'].map(us_state_abbrev)
+methane_percow = methane_percow[['State','Dairy Cows']]
+methane_percow.rename(columns={'Dairy Cows':'CH4/Cow'}, inplace=True)
+methane_percow.set_index('State', inplace=True)
+methane_percow['MTCO2e/Cow'] = methane_percow['CH4/Cow']*84
+
 num_cows_digest = digester_db['num_cows'].sum()
 print(num_cows_digest)
 
 total_em_reduct = digester_db['em_reduct'].sum()
 print(total_em_reduct)
+
 
 
